1a75d848ad70ff85adef3da44d1d183b0f9db3d5
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-09-01 22:02:00 +0700

    source code template dashboard dreamguy

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-// import ResetLocation from "../helpers/ResetLocation";
-
-const LinkButton = ({className, to, onClick, children}) => {
-    return <Link onClick={onClick} className={className} to={to}>{children}</Link>;
-}
-
-export default LinkButton;
